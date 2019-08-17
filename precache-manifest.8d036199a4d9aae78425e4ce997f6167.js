8ceda6d67c0ec84fcbda82dbd59725d0c309b5f6
Keshav Ashiya <keshavashiya@outlook.com>
2019-08-17 09:13:49 +0530

    Updates

---
+++ precache-manifest.8d036199a4d9aae78425e4ce997f6167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8040a9f84609a5f3ca7b796be8dbd77",
+    "url": "/randomuser/index.html"
+  },
+  {
+    "revision": "3335c0d1165f9d3ac67c",
+    "url": "/randomuser/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ca08cd2e4a726e516c06",
+    "url": "/randomuser/static/js/2.6a14516f.chunk.js"
+  },
+  {
+    "revision": "3335c0d1165f9d3ac67c",
+    "url": "/randomuser/static/js/main.cdd66225.chunk.js"
+  },
+  {
+    "revision": "885cec3ed5b0a62909df",
+    "url": "/randomuser/static/js/runtime~main.24e62ad1.js"
+  }
+]);
