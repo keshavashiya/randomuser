ea9b669a01df7fb9da4a9caf0ba12189d06c27a0
Keshav Ashiya <keshavashiya@outlook.com>
2019-08-17 11:08:10 +0530

    Design done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
         this.setState({
           user: responseJson
         });
-        console.log(this.state.user.results[0].gender);
+        // console.log(this.state.user.results[0].gender);
       })
       .catch(error => {
         console.error(error);
@@ -59,6 +59,7 @@ export default class App extends Component {
                 }}
               >
                 <div style={{ display: "flex", flexDirection: "row" }}>
+                  <h5>Name : &nbsp;</h5>
                   <h5>
                     {this.state.user.results[0].name.title}
                     &nbsp;&nbsp;&nbsp;&nbsp;
@@ -72,10 +73,62 @@ export default class App extends Component {
                     &nbsp;&nbsp;&nbsp;&nbsp;
                   </h5>
                 </div>
-                <h5>{this.state.user.results[0].email}</h5>
+                <h5>Mobile: &nbsp;{this.state.user.results[0].phone}</h5>
+                <h5>Cell: &nbsp;{this.state.user.results[0].cell}</h5>
+                <h5>email : &nbsp;{this.state.user.results[0].email}</h5>
+                <h5>gender : &nbsp;{this.state.user.results[0].gender}</h5>
+                <h5>
+                  Date of Birth : &nbsp;{this.state.user.results[0].dob.date}
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                </h5>
+                <h5>
+                  Age : &nbsp;{this.state.user.results[0].dob.age}&nbsp;&nbsp;&nbsp;&nbsp;
+                </h5>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <h5>Address : &nbsp;</h5>
+                  <h5>
+                    {this.state.user.results[0].location.street}{","}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                  <h5>
+                    {this.state.user.results[0].location.city}{","}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                  <h5>
+                    {this.state.user.results[0].location.state}{","}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                  <h5>
+                    {this.state.user.results[0].location.postcode}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <h5>Location : &nbsp;</h5>
+                  <h5>
+                    {this.state.user.results[0].location.coordinates.latitude}{","}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                  <h5>
+                    {this.state.user.results[0].location.coordinates.longitude}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <h5>Time zone : &nbsp;</h5>
+                  <h5>
+                    {this.state.user.results[0].location.timezone.offset}{","}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                  <h5>
+                    {this.state.user.results[0].location.timezone.description}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                  </h5>
+                </div>
+                <h5>{this.state.user.results[0].id.name} : {this.state.user.results[0].id.value}</h5>
               </div>
             </div>
-            <div style={{ display: "flex", flexDirection: "row" }}>
+            {/* <div style={{ display: "flex", flexDirection: "row" }}>
               <div
                 style={{
                   flexDirection: "column",
@@ -83,7 +136,7 @@ export default class App extends Component {
                   padding: 12
                 }}
               >
-                <h5>{this.state.user.results[0].gender}</h5>
+                <h5>gender : &nbsp;{this.state.user.results[0].gender}</h5>
               </div>
               <div
                 style={{
@@ -102,10 +155,17 @@ export default class App extends Component {
                   </h5>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         ) : (
-          <div style={{display: "flex", flexDirection: "row", justifyContent: "center", textAlign: "center"}}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              textAlign: "center"
+            }}
+          >
             <h6>Loading</h6>
           </div>
         )}
