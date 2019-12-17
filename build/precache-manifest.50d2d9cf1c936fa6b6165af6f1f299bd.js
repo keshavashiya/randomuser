8d3cff74b3dede652a4833b5e079fac3ef49c65f
Charan <charan@capdice.com>
2019-12-17 10:27:16 +0530

    Issue fixed

---
+++ build/precache-manifest.50d2d9cf1c936fa6b6165af6f1f299bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59568999de81a4fe5a9e3087125e527c",
+    "url": "/randomuser/index.html"
+  },
+  {
+    "revision": "9995d103c69ea834ff18",
+    "url": "/randomuser/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "ca08cd2e4a726e516c06",
+    "url": "/randomuser/static/js/2.6a14516f.chunk.js"
+  },
+  {
+    "revision": "9995d103c69ea834ff18",
+    "url": "/randomuser/static/js/main.c9aa7919.chunk.js"
+  },
+  {
+    "revision": "885cec3ed5b0a62909df",
+    "url": "/randomuser/static/js/runtime~main.24e62ad1.js"
+  }
+]);
